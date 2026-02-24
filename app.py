f7f4796089156bc75652e00824f18d623e8bb839
pradeepknl2023-code <pradeepknl2023@gmail.com>
2026-02-24 20:30:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ STRICT RULES:
 """
 
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+      model="llama-3.1-8b-instant",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
