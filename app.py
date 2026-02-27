44ed00dcfb262204a1b4302b7bdd5f8f2505025c
pradeepknl2023-code <pradeepknl2023@gmail.com>
2026-02-27 22:36:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 
 response = completion(
-    model="gemini/gemini-1.5-flash",
+    model="google/gemini-1.5-flash"
     messages=[{"role": "user", "content": "Hello"}],
     api_key=os.environ.get("GEMINI_API_KEY")
 )
